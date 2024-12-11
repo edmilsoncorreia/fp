29c4a1cc7f94a4e5dbcd80098116ebe737b099a6
Edmilson Correia <edmilson.cc@gmail.com>
2024-12-11 05:38:22 -0300

    added scripts.js

---
+++ scripts.js
@@ -0,0 +1,18 @@
+let prevButton = document.getElementById('prev')
+let nextButton = document.getElementById('next')
+let container = document.querySelector('.container')
+let items = container.querySelectorAll('.list .item')
+let indicators = querySelector('.indicators')
+let dots = document.querySelectorAll('ul li')
+
+prevButton.onclick = () => {
+
+    console.log("Próximo")
+
+}
+
+prevButton.onclick = () => {
+
+    console.log("Anterior")
+    
+}
