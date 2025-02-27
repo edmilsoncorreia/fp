ff285bf4ca0f62bba8984ae551f2a13bd69930fa
Edmilson Correia <edmilson.cc@gmail.com>
2025-02-27 04:43:14 -0300

    update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,17 +2,18 @@ let prevButton = document.getElementById('prev')
 let nextButton = document.getElementById('next')
 let container = document.querySelector('.container')
 let items = container.querySelectorAll('.list .item')
-let indicators = querySelector('.indicators')
+let indicators = document.querySelector('.indicators')
 let dots = document.querySelectorAll('ul li')
+let active = 0
+let firstPosition = 0
+let lastPosition = items.length -1
 
-prevButton.onclick = () => {
 
-    console.log("Próximo")
-
+nextButton.onclick = () => {
+    let itemOld = container.querySelector('.list .item.active')
+    itemOld.classList.remove ('active')
 }
 
 prevButton.onclick = () => {
-
-    console.log("Anterior")
-    
+    console.log("Anterior")    
 }
