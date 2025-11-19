b859482f95f2e3f083d325786c8a2fd89965d4f6
Edmilson Correia <edmilson.cc@gmail.com>
2025-11-19 04:56:43 -0300

    Atualização

--- scripts.js
+++ scripts.js
@@ -2,8 +2,9 @@ let prevButton = document.getElementById('prev')
 let nextButton = document.getElementById('next')
 let container = document.querySelector('.container')
 let items = container.querySelectorAll('.list .item')
-let indicators = document.querySelector('.indicators')
+let indicator = document.querySelector('.indicators')
 let dots = document.querySelectorAll('ul li')
+
 let active = 0
 let firstPosition = 0
 let lastPosition = items.length -1
@@ -11,20 +12,17 @@ let lastPosition = items.length -1
 
 nextButton.onclick = () => {
 
-    //Tentativa Alterar Active Dots
-
 
     let itemOld = container.querySelector('.list .item.active')
     itemOld.classList.remove ('active')
 
-    //let liOld = indicators.querySelector('ul li.active')
-    //liOld.classList.remove ('active')
-    
-    active = active >= lastPosition ? 0 : active + 1
-
+    active = active + 1 > lastPosition ? 0 : active + 1
     items[active].classList.add('active')
 
-    //dots[active].classList.add('.active')
+    let dotsOld = indicator.querySelector('ul li.active')
+    dotsOld.classList.remove ('active')
+
+    dots[active].classList.add('active')
 
     console.log(active)
 }
@@ -33,10 +31,9 @@ prevButton.onclick = () => {
     let itemOld = container.querySelector('.list .item.active')
     itemOld.classList.remove ('active')
 
-    active = active <= firstPosition ? lastPosition : active - 1
+    active = active - 1 < firstPosition ? lastPosition : active - 1
     items[active].classList.add('active')
 
     console.log(active)
 
-    
 }
