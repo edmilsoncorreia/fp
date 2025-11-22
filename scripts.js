16dc2c42b3a65df46804a17bc90c6c6f2adf86d6
Edmilson Correia <edmilson.cc@gmail.com>
2025-11-22 05:00:18 -0300

    Update Indicator UL LI Dots nextButton

--- scripts.js
+++ scripts.js
@@ -3,16 +3,13 @@ let nextButton = document.getElementById('next')
 let container = document.querySelector('.container')
 let items = container.querySelectorAll('.list .item')
 let indicator = document.querySelector('.indicators')
-let dots = document.querySelectorAll('ul li')
+let dots = document.querySelectorAll('.indicators ul li')
 
 let active = 0
 let firstPosition = 0
 let lastPosition = items.length -1
 
-
 nextButton.onclick = () => {
-
-
     let itemOld = container.querySelector('.list .item.active')
     itemOld.classList.remove ('active')
 
@@ -21,10 +18,7 @@ nextButton.onclick = () => {
 
     let dotsOld = indicator.querySelector('ul li.active')
     dotsOld.classList.remove ('active')
-
     dots[active].classList.add('active')
-
-    console.log(active)
 }
 
 prevButton.onclick = () => {
